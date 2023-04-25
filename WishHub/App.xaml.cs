b3d907562956efbee2dd55f057a297b0228d7d54
Luis David Lopez Zapata <luisdavidlz0202@gmail.com>
2023-04-24 23:07:18 -0500

    initial project files added

---
+++ WishHub/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WishHub;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ WishHub/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WishHub;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
